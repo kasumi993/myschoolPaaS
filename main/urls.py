ace09cc9888f1dfb2892cab9b3d24fb6ea5a4bba
kasumi993 <khadijag993@gmail.com>
2024-04-26 16:33:29 +0200

    rename main app
    need to configure app in settings

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myschoolpaas project.
+URL configuration for main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,8 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 
