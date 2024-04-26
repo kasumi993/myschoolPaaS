b8f4ea5fbb0864a757032aee52ac08d19c95aeff
kasumi993 <khadijag993@gmail.com>
2024-04-26 12:20:19 +0200

    configure urls

--- myschoolpaas/urls.py
+++ myschoolpaas/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.loginPage, name='login'),
+    path('student/profile/', views.studentProfile, name='student_profile'),
+    path('teacher/profile/', views.teacherProfile, name='teacher_profile'),
 ]
